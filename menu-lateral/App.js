b5c4f6ceebcd220fbe68e6aaa3f0fbb9e65b8540
gustavonunesx <gu.nunesx@gmail.com>
2025-09-17 14:40:55 -0300

    menu lateral

---
+++ menu-lateral/App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler'; // sempre primeiro!
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// Importa telas
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+// Cria o Drawer (menu lateral)
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Início">
+        <Drawer.Screen name="Início" component={HomeScreen} />
+        <Drawer.Screen name="Configurações" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
